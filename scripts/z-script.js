a6c64a098e9dafb3821ef7196968a53c7fa94d02
charlotte <charlottewu1994@gmail.com>
2016-02-15 16:53:23 +0800

    erttr

--- scripts/z-script.js
+++ scripts/z-script.js
@@ -26,29 +26,42 @@ function onPageChanged(index){
 		$(this).css('animation-name','');
 		$(this).css('-webkit-animation-play-state','paused');
 		$(this).css('-webkit-animation-name','');
-	})
+	});
 	
 	$('.sec' + index + ' .animationItem').each(function (){
 		var ani=$(this).data('animation');
 		$(this).css('animation-play-state','');
 		$(this).css('animation-name',ani);
 		$(this).css('visibility','visible');
-		$(this).css('-webkit-animation-play-state','')
-		$(this).css('-webkit-animation-name',ani);
-		
-	})
+		$(this).css('-webkit-animation-play-state','');
+		$(this).css('-webkit-animation-name',ani); 
+	});
     
 }
 
 /*-------------------first load the dom you want -----------------------*/
-function firstLoad(){
+/*
+使用data-* 属性到当前的页面进行填充src=“*”属性
+减少线程的堵塞 提高页面的加载速度
+*/
 
+function firstLoad(index){ 
+	$('.sec' + index + ' .img').each(function (){
+		var image = $(this).data('image');
+		$(this).attr('src',image); 
+	})
+
+}
+
+function everyPage(index){
+	onPageChanged(index);
+	firstLoad(index);
 }
 
 var pageSlider;
  
-	var query=GetRequest(); 
-	var dynamicID=query['id'];//获取动态场景ID
+var query=GetRequest(); 
+var dynamicID=query['id'];//获取动态场景ID
 	/*
 		通过Ajax读取json数据包
 	*/ 
@@ -56,7 +69,7 @@ var pageSlider;
 pageSlider = PageSlider.case({ 
 			duration:350,
 			elastic:false, 
-			callback:onPageChanged ,
+			callback:everyPage,
 			pageSwiching:function(e){ 
 				var e = window.event || event;
 				e.preventDefault();
@@ -68,9 +81,7 @@ pageSlider = PageSlider.case({
 				$('#box').removeClass('stopAnimation')
 			}
 		}
-	); 
-
-pageSlider.go(1);
+	);  
 
 $('.music').on('touchend',function(){
 	 if($('.music').hasClass('play')){
