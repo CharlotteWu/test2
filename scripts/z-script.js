21ddbb5337c6517c5d2add5991b5d010baa88522
charlotte <charlottewu1994@gmail.com>
2016-02-17 10:18:46 +0800

    dffd

--- scripts/z-script.js
+++ scripts/z-script.js
@@ -31,32 +31,49 @@ function onPageChanged(index){
 	$('.sec' + index + ' .animationItem').each(function (){
 		var ani=$(this).data('animation');
 		$(this).css('animation-play-state','');
-		$(this).css('animation-name',ani);
-		$(this).css('visibility','visible');
+		$(this).css('animation-name',ani); 
 		$(this).css('-webkit-animation-play-state','');
 		$(this).css('-webkit-animation-name',ani); 
+		$(this).css('visibility','visible');
 	});
     
 }
 
 /*-------------------first load the dom you want -----------------------*/
 /*
-使用data-* 属性到当前的页面进行填充src=“*”属性
+使用data-* 属性到当前的页面进行填充src*属性
 减少线程的堵塞 提高页面的加载速度
+通过ajax加载图片 通过返回标识码表示是否存在文件
+有则加载 反之则加载404图片
 */
 
 function firstLoad(index){ 
-	$('.sec' + index + ' .img').each(function (){
+	$('.sec' + index + ' .img').each(function (){ 
+
 		var image = $(this).data('image');
-		$(this).attr('src',image); 
-	})
 
-}
+		$.ajax({   
+			type:"GET",
+			data:image, 
+			success:function(data) { 
+				alert("haha");
+				$(this).attr('src',image); 
+			},
+			error:function(data) {
+				alert(data);
+				$(this).attr('src','imgs/404.jpg');
+			}  
+		}); 
 
+	}); 
+} 
+
+/*all callback function in here*/
 function everyPage(index){
 	onPageChanged(index);
 	firstLoad(index);
 }
+/*-----------------------------*/
 
 var pageSlider;
  
@@ -82,6 +99,7 @@ pageSlider = PageSlider.case({
 			}
 		}
 	);  
+
 
 $('.music').on('touchend',function(){
 	 if($('.music').hasClass('play')){
